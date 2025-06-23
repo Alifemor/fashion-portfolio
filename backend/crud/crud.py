4d6aa998ffa4338cf50effebf4973025058c37a5
Alifemor <65672456+Alifemor@users.noreply.github.com>
2025-06-23 14:54:40 +0300

    Update crud.py

--- backend/crud/crud.py
+++ backend/crud/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from backend import models, schemas
+from models import db_models as models
+from schemas import model_schemas, review_schemas
 
 # ======== МОДЕЛИ ОБУВИ ========
 
@@ -9,14 +10,18 @@ def get_models(db: Session):
 def get_model(db: Session, model_id: int):
     return db.query(models.ShoeModel).filter(models.ShoeModel.id == model_id).first()
 
-def create_model(db: Session, model: schemas.ShoeModelCreate):
+def create_model(db: Session, model: model_schemas.ShoeModelCreate):
     db_model = models.ShoeModel(**model.dict())
     db.add(db_model)
     db.commit()
     db.refresh(db_model)
     return db_model
 
-
 # ======== ОТЗЫВЫ ========
 
-def create_review(db: Session, model_id: int, review: schemas.ReviewCreate
+def create_review(db: Session, model_id: int, review: review_schemas.ReviewCreate):
+    db_review = models.Review(shoe_model_id=model_id, **review.dict())
+    db.add(db_review)
+    db.commit()
+    db.refresh(db_review)
+    return db_review
