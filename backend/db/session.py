0f498db1608aec2148e443e4181145ab75f29c5c
Alifemor <65672456+Alifemor@users.noreply.github.com>
2025-06-23 15:31:17 +0300

    Update session.py

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from core.config import settings  # Убираем backend.
+from core.config import settings  
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
