f6ab3f73401e1f70d34d6fdda13e3d9b97b75d27
Alifemor <65672456+Alifemor@users.noreply.github.com>
2025-06-23 12:32:43 +0300

    Update schemas.py

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -1,1 +1,42 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+
+# ======= ОТЗЫВЫ =======
+
+class ReviewBase(BaseModel):
+    name: str
+    rating: int = Field(..., ge=1, le=5)
+    comment: Optional[str] = None
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class ReviewOut(ReviewBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+# ======= МОДЕЛИ ОБУВИ =======
+
+class ShoeModelBase(BaseModel):
+    name: str
+    description: str
+    photo_urls: List[str]
+    tags: List[str]
+
+class ShoeModelCreate(ShoeModelBase):
+    pass
+
+class ShoeModelOut(ShoeModelBase):
+    id: int
+    created_at: datetime
+    avg_rating: Optional[float]
+    num_reviews: int
+
+    class Config:
+        orm_mode = True
 
