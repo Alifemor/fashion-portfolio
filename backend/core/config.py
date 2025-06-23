1cc3565c2582fad184c80469b51a22489b448e67
Alifemor <65672456+Alifemor@users.noreply.github.com>
2025-06-23 15:26:43 +0300

    Update config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -2,11 +2,12 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Designer Portfolio"
-    API_KEY: str = "supersecretkey"  # поменяешь при необходимости
+    API_KEY: str = "supersecretkey"
     DATABASE_URL: str = "postgresql://user:password@localhost:5432/portfolio_db"
     REDIS_URL: str = "redis://localhost:6379"
-
+    DEBUG: bool = True  
+    
     class Config:
-        env_file = ".env"  # можно переопределить переменные через .env
+        env_file = ".env"
 
 settings = Settings()
